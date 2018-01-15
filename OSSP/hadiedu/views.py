7da7c17ec29ff943c2f964997e4d1ee3e0a22ebb
김민중 <kl4314@likelion.org>
2018-01-15 14:25:32 +0900

    알고리즘 변경

--- OSSP/hadiedu/views.py
+++ OSSP/hadiedu/views.py
@@ -40,29 +40,30 @@ def input(request):
   rdr = csv.reader(f)
 
   for row in rdr:
-    line.insert(0,row)
-    for lines in line:
-      school = School(
-        eduoffice = lines[0],
-        code = lines[3],
-        sname = lines[4],
-        diff= lines[6],
-        Enter  = lines[9],
-        parking = lines[10],
-        enhi  = lines[11],
-        hallway = lines[12],
-        hsupport = lines[13],
-        hfeces = lines[14],
-        hufine = lines[15],
-        braille = lines[16],
-        announce = lines[17],
-        alarm = lines[18],
-        address = lines[1],
-        daddress = lines[2],
-        pnum = lines[7],
-        haddress = lines[5],
-        sex = lines[8],
-        )
-      school.save()
+    line.append(row)
+    
+  for lines in line:
+    school = School(
+      eduoffice = lines[0],
+      code = lines[3],
+      sname = lines[4],
+      diff= lines[6],
+      Enter  = lines[9],
+      parking = lines[10],
+      enhi  = lines[11],
+      hallway = lines[12],
+      hsupport = lines[13],
+      hfeces = lines[14],
+      hufine = lines[15],
+      braille = lines[16],
+      announce = lines[17],
+      alarm = lines[18],
+      address = lines[1],
+      daddress = lines[2],
+      pnum = lines[7],
+      haddress = lines[5],
+      sex = lines[8],
+     )
+    school.save()
 
-  return render(request, 'hadiedu/index.html')
+return render(request, 'hadiedu/index.html')
